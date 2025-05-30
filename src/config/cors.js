9aee7b136470e1b8471f068165fe3b1aa2c27b59
gulsherkooner <gulsher.kooner@gmail.com>
2025-05-30 09:31:42 +0530

    .

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 
 const corsOptions = {
-  origin: ['https://next-frontend-one-xi.vercel.app', 'http://135.181.192.55:3000', 'https://post-microservice.vercel.app','http://135.181.192.55:3004'],
+  origin: ['http://next-frontend:3000', 'http://post-service:3004'],
   credentials: true, // Allow cookies (e.g., refreshToken)
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS','PATCH'],
   allowedHeaders: ['X-Requested-With, Content-Type, Authorization'],
