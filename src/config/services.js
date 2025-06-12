3ba199355c0a815fadb2dca5e0404edf2a16af95
Cyros-Sachin <sachinkc4456@gmail.com>
2025-06-12 16:48:56 +0530

    message

--- src/config/services.js
+++ src/config/services.js
@@ -2,5 +2,6 @@ module.exports = {
   'auth-service': process.env.AUTH_SERVICE_URL || 'http://localhost:3002',
   'post-service': process.env.POST_SERVICE_URL || 'http://localhost:3004',
   'dating-service': process.env.DATING_SERVICE_URL || 'http://localhost:5000', // add this line
+  'message-service': process.env.MESSAGE_SERVICE_URL || 'http://localhost:5000', // add this line
   'sub-service': process.env.SUBSCRIPTION_SERVICE_URL || 'http://localhost:3005',
 };
---
+++ src/routes/message.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const { authenticateAccessToken } = require('../middleware/auth');
+const forwardRequest = require('../utils/forwardRequest');
+const logger = require('../config/logger');
+const { verifyAccessToken } = require('../utils/jwt');
+
+const router = express.Router();
+
+router.post('/upload-audio', authenticateAccessToken, express.json(), async (req, res) => {
+  logger.info('Handling POST /messages/upload-audio request', { user_id: req.user?.user_id });
+  req.headers['x-user-id'] = req.user?.user_id || '';
+  await forwardRequest(req, res, 'message-service', 'upload-audio');
+});
+router.post('/upload-image', authenticateAccessToken, express.json(), async (req, res) => {
+  logger.info('Handling POST /messages/upload-audio request', { user_id: req.user?.user_id });
+  req.headers['x-user-id'] = req.user?.user_id || '';
+  await forwardRequest(req, res, 'message-service', 'upload-image');
+});
+
+router.get('/conversation/:partnerId/:userId', authenticateAccessToken, async (req, res) => {
+  logger.info('Handling GET /conversation/:partnerId/:userId request', { user_id: req.params.userId });
+  req.headers['x-user-id'] = req.params.userId || '';
+  await forwardRequest(req, res, 'message-service', `conversation/${req.params.partnerId}/${req.params.userId}`);
+});
+
+router.get('/unread-counts/:userId', authenticateAccessToken, async (req, res) => {
+  logger.info('Handling GET /unread-counts/:userId request', { user_id: req.params.userId });
+  req.headers['x-user-id'] = req.params.userId || '';
+  await forwardRequest(req, res, 'message-service', `unread-counts/${req.params.userId}`);
+});
+
+router.put('/mark-read/:fromId/:userId', authenticateAccessToken, express.json(), async (req, res) => {
+  logger.info(`Handling PUT /mark-read/:fromId/:userId request`, { user_id: req.params.userId });
+  req.headers['x-user-id'] = req.params.userId || '';
+  await forwardRequest(req, res, 'message-service', `mark-read/${req.params.fromId}/${req.params.userId}`, 'PUT');
+});
+
+module.exports = router;
